37fd3d52ee356d3d96f28c88e679a6b0d3a755ff
Abdul Khaliq <abdulkhaliq94@gmail.com>
2020-12-12 23:57:10 -0500

    rendering react-router in app.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
 import './App.css';
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
-import Main from './components/main';
-import { Link } from 'react-router-dom';
+import { Layout, Content } from 'react-mdl';
+import Main from './pages/Main';
 
 function App() {
   return (
-    // <div className="demo-big-content">
-    <div>
-      <Layout>
-          <Header title="Title" scroll>
-              <Navigation>
-                  <Link to="/home">Home</Link>
-                  <Link to="/about">About</Link>
-                  <Link to="/projects">Projects</Link>
-                  <Link to="/contact">Contact</Link>
-              </Navigation>
-          </Header>
-          <Drawer title="Title">
-              <Navigation>
-                  <Link to="/home">Home</Link>
-                  <Link to="/about">About</Link>
-                  <Link to="/projects">Projects</Link>
-                  <Link to="/contact">Contact</Link>
-              </Navigation>
-          </Drawer>
-          <Content>
-              <div className="page-content" />
-              <Main />
-          </Content>
-      </Layout>
-    </div>
+      <div>
+        <Layout>
+            <Content>
+
+                {/** React Router setup */}
+                
+                <Main />
+            </Content>
+        </Layout>
+      </div>
   );
 }
 
