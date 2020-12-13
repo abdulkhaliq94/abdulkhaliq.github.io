3a80bdcf13069880309b873aa3206c424fba4490
Abdul Khaliq <abdulkhaliq94@gmail.com>
2020-12-13 00:00:22 -0500

    created the sidebar or hamburger menu for mobile layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Layout, /*Header,*/ Navigation, Drawer, Content } from 'react-mdl';
 import Main from './pages/Main';
+import { Link } from 'react-router-dom';
 import Navbar from './components/Navbar'
 
 function App() {
@@ -14,6 +15,14 @@ function App() {
                 
                 {/** Sidebar */}
                 
+                <Drawer>
+                    <Navigation>
+                        <Link to="/Home">Home</Link>
+                        <Link to="/About">About</Link>
+                        <Link to="/Projects">Projects</Link>
+                        <Link to="/Contact">Contact</Link>
+                    </Navigation>
+                </Drawer>
             <Content>
 
                 {/** React Router setup */}
