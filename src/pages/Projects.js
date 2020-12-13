1c1bb12982385020fd458271509332ccacd5bb46
Abdul Khaliq <abdulkhaliq94@gmail.com>
2020-12-13 00:47:05 -0500

    adding cards for react tab

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -14,7 +14,28 @@ class projects extends Component {
         if(this.state.activeTab === 0) {
             return (
                 <div className="grid-flow-horizontal">
-                    
+                    <div className="cards">
+                        <Card shadow={5} style={{minWidth: '450', height: '100%', margin: 'auto'}}>
+                            <CardTitle style={{color: 'white', height: '176px', display: 'flex', background: 
+                                'url(https://i.redd.it/x1sr1lob3ai41.jpg) center / cover'}}>
+                                    
+                                </CardTitle>
+                            <CardText id="card-text">
+                                    <p>Unknown</p>
+                                    <i>Coming soon...</i>
+
+                                    <br />Pellentesque posuere neque velit, nec condimentum urna efficitur nec. 
+                                    Fusce ante felis, mollis in nisl nec, pharetra viverra mi. 
+                                    In malesuada dictum ex quis scelerisque.
+                                    <br /><br />Technologies used: HTML | CSS | REACT | BOOTSTRAP
+                                </CardText>
+                            <CardActions border style={{textDecoration: "none"}}>
+                                    <Button colored>CodePen</Button>
+                                    <Button colored>LiveDemo</Button>
+                                    <Button colored>Github</Button>
+                                </CardActions>
+                        </Card>
+                    </div>
                 </div>
             )
         } else if(this.state.activeTab === 1) {
