aa24e6f76e26ba96c219478f434b6fc86575011e
Abdul Khaliq <abdulkhaliq94@gmail.com>
2020-12-13 00:06:52 -0500

    added the background for the about page

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css'
+
+class about extends Component {
+    render() {
+        return(
+                <div className="canva">
+                        
+                </div>
+        )
+    }
+}
+
+export default about;
