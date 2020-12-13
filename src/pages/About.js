2afe0a6630f33f2f3a3bc1f3f476ad455dee2d3a
Abdul Khaliq <abdulkhaliq94@gmail.com>
2020-12-13 00:08:39 -0500

    added an image (svg of a programmer)

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import '../App.css'
+import Hacker from '../images/hacker.svg'
 
 class about extends Component {
     render() {
         return(
                 <div className="canva">
-                        
+                        <div className="container">
+                            <div className="image">
+                                <img src={Hacker} alt="_blank"/>
+                            </div>
+
+                        </div>
                 </div>
         )
     }
