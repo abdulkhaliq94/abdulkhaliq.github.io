813303ba9dd189a8cb342819f3fcb2afdf24e654
Abdul Khaliq <abdulkhaliq94@gmail.com>
2020-12-14 01:18:13 -0500

    removed lock file

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,13 +5,26 @@ import About from './About';
 import Projects from './Projects';
 import Contact from './Contact';
 
-const Main = () => (
-    <Switch>
-        <Route path="/About" component={About} />
-        <Route path="/Projects" component={Projects} />
-        <Route path="/Contact" component={Contact} />
-        <Route path="/" component={Home} />
-    </Switch>
-)
+// const Main = () => (
+//     <Switch>
+//         <Route path="/About" component={About} />
+//         <Route path="/Projects" component={Projects} />
+//         <Route path="/Contact" component={Contact} />
+//         <Route path="/" component={Home} />
+//     </Switch>
+// )
+
+function Main() {
+    return(
+        <>
+            <Switch>
+                <Route path="/About"><About /></Route>
+                <Route path="/Projects"><Projects /></Route>
+                <Route path="/Contact"><Contact /></Route>
+                <Route path="/"><Home /></Route>
+            </Switch>
+        </>
+    )
+}
 
 export default Main;
